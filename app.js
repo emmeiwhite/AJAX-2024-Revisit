f5c3a8a8af2e02f55970983a0807addbc3a98bdc
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-22 15:55:11 +0530

    A basic Promise Example. Asynchronous world is awesome

--- app.js
+++ app.js
@@ -23,6 +23,25 @@ topDiv.addEventListener('click', () => {
 
 /** --- Let's work a basic Promise example | Always think of Cab booking when thinking about Promises  --- */
 
-let promise = new Promise((resolve, reject) => {})
+let promise = new Promise((resolve, reject) => {
+  let value = false
 
-console.log(promise)
+  if (value) {
+    setTimeout(() => {
+      resolve('Got the Data to be used')
+    }, 3000)
+  } else {
+    setTimeout(() => {
+      reject('Error getting the data')
+    }, 3000)
+  }
+})
+
+console.log('Async code running in the background ...')
+promise
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
