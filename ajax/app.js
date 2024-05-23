b5ca03e0a1b41b4d2e3b7b9c8949d51442310fcb
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 20:30:16 +0530

    Using async-await with fetch, a much better and clean solution for asynchronous api calls

--- ajax/app.js
+++ ajax/app.js
@@ -1,51 +1,15 @@
-// fetch : Based on Promises and more cleaner
-
 const url = './api/people.json'
-
-/* ---
-fetch(url)
-  .then(response => {
-    // response object: It doesn't directly get us the data but some useful data
-    // useful properties and methods : We require json() method
-    // convert response to  JSON data with json() method
-    // It returns a promise as well, so we'll require another .then() to handle the Promise
-
-    return response.json()
-  })
-  .then(data => {
-    console.log(data)
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  ---*/
-
-/* ---
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-    .catch(error => console.log(error))
-    --- */
-
-/** Let's work on our button example:
- * we'll click on the button and fetch the external data (in our case json data)
- * in our api folder
- */
 
 const btn = document.querySelector('button')
 
-btn.addEventListener('click', () => {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(typeof data)
+btn.addEventListener('click', async () => {
+  const response = await fetch(url)
+  const data = await response.json()
 
-      const htmlParas = data
-        .map(person => {
-          return `<p>${person.name}</p>`
-        })
-        .join('')
-      document.body.insertAdjacentHTML('beforeend', htmlParas)
+  const htmlParas = data
+    .map(person => {
+      return `<p>${person.name}</p>`
     })
-    .catch(err => console.log(err))
+    .join('')
+  document.body.insertAdjacentHTML('beforeend', htmlParas)
 })
