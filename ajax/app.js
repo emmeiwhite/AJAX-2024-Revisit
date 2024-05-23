e5ba60006782fe76d4c622a51790768c9b6485e3
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 18:50:15 +0530

    getting data on button click to show no page reload is happening when adding the data

--- ajax/app.js
+++ ajax/app.js
@@ -1,23 +1,33 @@
-const xhr = new XMLHttpRequest()
-console.log(xhr)
+const btn = document.querySelector('button')
 
-xhr.open('GET', './api/sample.txt')
+btn.addEventListener('click', () => {
+  getData()
+})
 
-xhr.onreadystatechange = function () {
-  console.log('ReadyState:', xhr.readyState)
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    // Check if the request is complete
-    console.log('Status:', xhr.status) // Log the status
-    if (xhr.status >= 200 && xhr.status < 300) {
-      // Check if the request was successful
-      const para = document.createElement('p')
-      para.textContent = xhr.responseText
+const getData = () => {
+  const xhr = new XMLHttpRequest()
 
-      document.body.appendChild(para)
-      console.log('Response:', xhr.responseText) // Log the response text
-    } else {
-      console.error('Request failed with status:', xhr.status) // Handle error
+  xhr.open('GET', './api/sample.txt')
+
+  xhr.onreadystatechange = function () {
+    console.log('ReadyState:', xhr.readyState)
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Check if the request is complete
+      console.log('Status:', xhr.status) // Log the status
+      if (xhr.status >= 200 && xhr.status < 300) {
+        // Check if the request was successful
+        const para = document.createElement('p')
+        para.textContent = xhr.responseText
+
+        document.body.appendChild(para)
+        console.log('Response:', xhr.responseText) // Log the response text
+      } else {
+        console.error('Request failed with status:', xhr.status) // Handle error
+      }
     }
   }
+  xhr.send()
 }
-xhr.send()
+
+// This line of code runs at the start because the XHR code runs asynchronously in the background
+console.log('Hello World!')
