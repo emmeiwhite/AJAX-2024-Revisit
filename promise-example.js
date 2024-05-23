c01bf29415616edf194d60828f11c76e8bdb5885
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 16:10:56 +0530

    really enjoyed this example in pure JS

--- promise-example.js
+++ promise-example.js
@@ -1,24 +1,41 @@
 // Let's upload the image on button click
 const btn = document.querySelector('.btn')
 const container = document.querySelector('.img-container')
+const loader = document.querySelector('.loader')
+const error = document.querySelector('.error')
 
-const url = 'https://source.unsplash.com'
+const url = 'https://source.unsplash.com/random'
 
 btn.addEventListener('click', () => {
+  loader.style.display = 'block'
   const promise = loadImage(url)
-  console.log(promise)
+  promise
+    .then(data => {
+      loader.style.display = 'none'
+      error.style.display = 'none'
+      console.log(data)
+      container.innerHTML = data
+    })
+    .catch(errorText => {
+      loader.style.display = 'none'
+      error.innerHTML = `<p>${errorText}</p>`
+      error.style.display = 'block'
+    })
 })
 
 function loadImage(urlImage) {
   return new Promise((resolve, reject) => {
     let img = new Image()
+    img.classList.add('img')
 
     img.addEventListener('load', () => {
       console.log('Hey everything worked!')
+      resolve(img)
     })
 
     img.addEventListener('error', () => {
       console.log('There was an error!')
+      reject(new Error(`Failed to load the image from the source:${urlImage}`))
     })
 
     img.src = urlImage // here we are setting src for the image
