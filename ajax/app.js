272cf523aae43600468864e7b79baf9efba4ca4d
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 19:30:32 +0530

    Communication b/w server happens in text and we need to parse JSON string format to array

--- ajax/app.js
+++ ajax/app.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('button')
-
+const url = './api/people.json'
 btn.addEventListener('click', () => {
-  getData()
+  getData(url)
 })
 
-const getData = () => {
+const getData = url => {
   const xhr = new XMLHttpRequest()
 
-  xhr.open('GET', './api/sample.txt')
+  xhr.open('GET', url)
 
   xhr.onreadystatechange = function () {
     console.log('ReadyState:', xhr.readyState)
@@ -16,11 +16,16 @@ const getData = () => {
       console.log('Status:', xhr.status) // Log the status
       if (xhr.status >= 200 && xhr.status < 300) {
         // Check if the request was successful
+        /*
         const para = document.createElement('p')
         para.textContent = xhr.responseText
 
         document.body.appendChild(para)
         console.log('Response:', xhr.responseText) // Log the response text
+        */
+
+        const data = JSON.parse(xhr.responseText)
+        console.log(data)
       } else {
         console.error('Request failed with status:', xhr.status) // Handle error
       }
