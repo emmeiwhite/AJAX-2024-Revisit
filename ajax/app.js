071d804129a0690a2c0de7f5f65b64bce8065c15
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 20:38:46 +0530

    Handling basic Error with try/catch

--- ajax/app.js
+++ ajax/app.js
@@ -3,13 +3,29 @@ const url = './api/people.json'
 const btn = document.querySelector('button')
 
 btn.addEventListener('click', async () => {
-  const response = await fetch(url)
-  const data = await response.json()
+  try {
+    const response = await fetch(url)
 
+    console.log(response)
+
+    if (!response.ok) {
+      throw new Error('Resource Not Found')
+    }
+    console.log('I am here')
+    const data = await response.json()
+
+    renderPersons(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+function renderPersons(data) {
   const htmlParas = data
     .map(person => {
       return `<p>${person.name}</p>`
     })
     .join('')
+
   document.body.insertAdjacentHTML('beforeend', htmlParas)
-})
+}
