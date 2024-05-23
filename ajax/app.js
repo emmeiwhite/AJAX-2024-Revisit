a3278c3f8d6ee714f4257faf6fb99600f82a3977
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-23 18:40:53 +0530

    We are getting data by the code running in the background without reloading the page

--- ajax/app.js
+++ ajax/app.js
@@ -2,3 +2,22 @@ const xhr = new XMLHttpRequest()
 console.log(xhr)
 
 xhr.open('GET', './api/sample.txt')
+
+xhr.onreadystatechange = function () {
+  console.log('ReadyState:', xhr.readyState)
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    // Check if the request is complete
+    console.log('Status:', xhr.status) // Log the status
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // Check if the request was successful
+      const para = document.createElement('p')
+      para.textContent = xhr.responseText
+
+      document.body.appendChild(para)
+      console.log('Response:', xhr.responseText) // Log the response text
+    } else {
+      console.error('Request failed with status:', xhr.status) // Handle error
+    }
+  }
+}
+xhr.send()
