abaaf22b374e32d633beb02f469b463cd8334df6
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-27 17:10:09 +0530

    side hussle

--- closures/app.js
+++ closures/app.js
@@ -34,3 +34,33 @@ const roufi = newHire('Mr. Rouf', 'QA Engineer')
 // This is still a closure
 console.log(rafs.getNewCandidate())
 console.log(roufi.getNewCandidate())
+
+/*
+Yes, there is no in-build method in FullCalendar.io, that directly retrieves only the "next" event. However we can achieve this by implementing a custom function that filters and sorts the events to find the next one.
+
+Below is how we can approach this problem:
+
+*/
+
+function getNextEvent() {
+  // Get current data
+  const now = new Date()
+  // Get all the events
+  const events = calendar.getEvents()
+
+  /* Filter events to include only those starting in the future */
+  const futureEvents = events.filter(event => event.start > now)
+  /** Sort Future events by their time */
+  futureEvents.sort((a, b) => a.start - b.start)
+
+  // Return the next event (which will be first event in the sorted list) or null if no future events
+  return futureEvents.length > 0 ? futureEvents[0] : null
+}
+
+/** let's use the function */
+const nextEvent = getNextEvent()
+if (nextEvent) {
+  console.log('Next event:', nextEvent.title, 'at', nextEvent.start)
+} else {
+  console.log('There are no upcoming events.')
+}
