4ca17e9bb55558b726293b9531d5ed98c5f7d5d5
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-27 14:41:47 +0530

    returning object instead of function directly still keeps inner function in closure

--- closures/app.js
+++ closures/app.js
@@ -1,7 +1,6 @@
 // Closure: In a higher order function in which a function returns another function, the inner function remembers the variables stored in the outer function even after the outer() function has executed.
 
 const setNewAccount = (accountHolder, initialBalance) => {
-  console.log('I am the outer function')
   return function () {
     return `Account Holder: ${accountHolder} has initial balance: ${initialBalance}`
   }
@@ -10,7 +9,28 @@ const setNewAccount = (accountHolder, initialBalance) => {
 const rawa = setNewAccount('rawdha', 300)
 const immi = setNewAccount('emmei', 200)
 
-console.log(rawa())
-console.log(immi())
+// console.log(rawa())
+// console.log(immi())
 
 // Even if we visit after 10 years, the inner function remembers the outer function variables
+/** Today, I learned a new thing that we can also return a method with the inner method as the property */
+
+const newHire = (candidate, designation) => {
+  console.log(candidate, designation)
+
+  function getNewCandidate() {
+    return `Candidate is ${candidate} & Designation is ${designation}`
+  }
+
+  // Instead of returning the inner function, let's return an object with that method as property
+  return {
+    getNewCandidate
+  }
+}
+
+const rafs = newHire('Rafia', 'Junior BE')
+const roufi = newHire('Mr. Rouf', 'QA Engineer')
+
+// This is still a closure
+console.log(rafs.getNewCandidate())
+console.log(roufi.getNewCandidate())
