6565b100a0c6fc827629e3bcf61f397f89cdd9d4
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-28 20:03:26 +0530

    learning some ES6 features find(), findIndex(), for in etc

--- es6-continue/app.js
+++ es6-continue/app.js
@@ -27,3 +27,35 @@ const person = {
 console.log(Array.from(person))
 
 /** find(), findIndex(), every(), some() */
+const persons = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+  { id: 4, name: 'Diana' },
+  { id: 5, name: 'Edward' }
+]
+
+const anna = persons.find(person => person.name === 'Alice')
+
+// indexOf()
+const indexOfDiana = persons.findIndex(person => person.name === 'Diana')
+
+const removedDianaFromArray = [
+  ...persons.slice(0, indexOfDiana),
+  ...persons.slice(indexOfDiana + 1)
+]
+
+// We could also directly remove using filter()
+const filteredPersons = persons.filter(person => person.name !== 'Diana')
+
+/** for in loop | iterate over object properties */
+const table = {
+  color: 'green',
+  height: 3,
+  width: 2
+}
+for (let parameter in table) {
+  //   console.log(parameter)
+  // we can iterate over properties in an object
+  console.log(`${parameter}: ${table[parameter]}`)
+}
